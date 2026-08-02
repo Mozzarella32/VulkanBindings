3a5a8df1938b01148e812e8c5e77b6ef186cebaa
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2026-08-02 11:19:30 +0200

    Chore: remove redundent concepts

--- bindings/include/VkBindings/Concepts.hpp
+++ bindings/include/VkBindings/Concepts.hpp
@@ -2,19 +2,7 @@
 
 namespace VkBindings::Concepts {
 template <typename T>
-concept HasDispatcher = T::supports_dispatcher;
-
-template <typename T>
 concept HasHandleConstructor = T::has_handle_constructor;
-
-template <typename T>
-concept IsPoolAllocated = T::is_pool_allocated;
-
-template <typename T>
-concept IsUnique = T::is_unique;
-
-template <typename T>
-concept IsObject = T::is_object;
 
 template <typename T>
 concept ABIIsHandle =
