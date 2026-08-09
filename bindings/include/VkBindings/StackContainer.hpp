368eb84e385318a530195b4a83c4ed03cf3c035d
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2026-08-09 21:38:39 +0200

    Feat: StackContainer

---
+++ bindings/include/VkBindings/StackContainer.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "Concepts.hpp"
+#include "VkBindings/Reflection/BitsToFlag.hpp"
+#include "VkBindings/Reflection/IsBits.hpp"
+#include "VkBindings/Reflection/IsEnum.hpp"
+#include "VkBindings/Reflection/IsFlag.hpp"
+#include "VkBindings/Reflection/IsObject.hpp"
+#include "VkBindings/Reflection/IsUnique.hpp"
+#include "VkBindings/private/StructTemplatesInterface.hpp"
+
+namespace VkBindings {
+
+template <Concepts::IsBits First, Concepts::IsBits... Ts>
+    requires(std::same_as<Ts, First> && ...)
+constexpr auto stackContainer(const First &first, const Ts &...ts)
+    -> std::array<Reflections::BitsToFlag<First>, 1 + sizeof...(Ts)> {
+    return {first, ts...};
+}
+
+template <Concepts::IsEnum First, Concepts::IsEnum... Ts>
+    requires(std::same_as<Ts, First> && ...)
+constexpr auto stackContainer(const First &first, const Ts &...ts)
+    -> std::array<First, 1 + sizeof...(Ts)> {
+    return {first, ts...};
+}
+
+template <Concepts::IsFlag First, Concepts::IsFlag... Ts>
+    requires(std::same_as<Ts, First> && ...)
+constexpr auto stackContainer(const First &first, const Ts &...ts)
+    -> std::array<First, 1 + sizeof...(Ts)> {
+    return {first, ts...};
+}
+
+template <Concepts::IsObject First, Concepts::IsObject... Ts>
+    requires(std::same_as<Ts, First> && ...)
+constexpr auto stackContainer(const First &first, const Ts &...ts)
+    -> std::array<impl_Struct::AssignableHandle<First>, 1 + sizeof...(Ts)> {
+    return {first.getHandle(), ts.getHandle()...};
+}
+
+template <Concepts::IsUnique First, Concepts::IsUnique... Ts>
+    requires(std::same_as<Ts, First> && ...)
+constexpr auto stackContainer(const First &first, const Ts &...ts)
+    -> std::array<impl_Struct::AssignableHandle<typename First::object_type>, 1 + sizeof...(Ts)> {
+    return {first.getHandle(), ts.getHandle()...};
+}
+
+} // namespace VkBindings
