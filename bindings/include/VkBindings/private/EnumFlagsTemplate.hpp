f605b2c9c95a2dc25ce260833dd7d62443efced4
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2026-08-29 10:26:02 +0200

    Fix: dont use Flags alias as it shaddows

--- bindings/include/VkBindings/private/EnumFlagsTemplate.hpp
+++ bindings/include/VkBindings/private/EnumFlagsTemplate.hpp
@@ -3,8 +3,6 @@
 #include <type_traits>
 #ifdef _MSC_VER
 #include <compare>
-#pragma warning(push)
-#pragma warning(suppress : 4459)
 #endif
 
 namespace VkBindings::impl_Enum {
@@ -59,56 +57,43 @@ template <class E>
 concept HasVkFlags = requires { typename impl_Enum::Flags<E>; };
 
 template <HasVkFlags E> constexpr auto operator|(E bitA, E bitB) -> impl_Enum::Flags<E> {
-    using Flags = impl_Enum::Flags<E>;
-    return Flags(bitA) | Flags(bitB);
+    return impl_Enum::Flags<E>(bitA) | impl_Enum::Flags<E>(bitB);
 }
 
 template <HasVkFlags E> constexpr auto operator&(E bitA, E bitB) -> impl_Enum::Flags<E> {
-    using Flags = impl_Enum::Flags<E>;
-    return Flags(bitA) & Flags(bitB);
+    return impl_Enum::Flags<E>(bitA) & impl_Enum::Flags<E>(bitB);
 }
 
 template <HasVkFlags E> constexpr auto operator^(E bitA, E bitB) -> impl_Enum::Flags<E> {
-    using Flags = impl_Enum::Flags<E>;
-    return Flags(bitA) ^ Flags(bitB);
+    return impl_Enum::Flags<E>(bitA) ^ impl_Enum::Flags<E>(bitB);
 }
 template <HasVkFlags E>
 constexpr auto operator|(E bit, impl_Enum::Flags<E> flag) -> impl_Enum::Flags<E> {
-    using Flags = impl_Enum::Flags<E>;
-    return Flags(bit) | flag;
+    return impl_Enum::Flags<E>(bit) | flag;
 }
 
 template <HasVkFlags E>
 constexpr auto operator&(E bit, impl_Enum::Flags<E> blag) -> impl_Enum::Flags<E> {
-    using Flags = impl_Enum::Flags<E>;
-    return Flags(bit) & blag;
+    return impl_Enum::Flags<E>(bit) & blag;
 }
 
 template <HasVkFlags E>
 constexpr auto operator^(E bit, impl_Enum::Flags<E> flag) -> impl_Enum::Flags<E> {
-    using Flags = impl_Enum::Flags<E>;
-    return Flags(bit) ^ flag;
+    return impl_Enum::Flags<E>(bit) ^ flag;
 }
 
 template <HasVkFlags E>
 constexpr auto operator|(impl_Enum::Flags<E> flag, E bit) -> impl_Enum::Flags<E> {
-    using Flags = impl_Enum::Flags<E>;
-    return flag | Flags(bit);
+    return flag | impl_Enum::Flags<E>(bit);
 }
 
 template <HasVkFlags E>
 constexpr auto operator&(impl_Enum::Flags<E> flag, E bit) -> impl_Enum::Flags<E> {
-    using Flags = impl_Enum::Flags<E>;
-    return flag & Flags(bit);
+    return flag & impl_Enum::Flags<E>(bit);
 }
 
 template <HasVkFlags E>
 constexpr auto operator^(impl_Enum::Flags<E> flag, E bit) -> impl_Enum::Flags<E> {
-    using Flags = impl_Enum::Flags<E>;
-    return flag ^ Flags(bit);
+    return flag ^ impl_Enum::Flags<E>(bit);
 }
 } // namespace VkBindings
-
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
