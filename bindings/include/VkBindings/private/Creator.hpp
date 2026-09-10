964c13e7adfcd462965f086a7debe6e41b9c4f3c
Valentin Mücklausch <valentin.muecklausch@outlook.de>
2026-09-10 19:18:54 +0200

    Feat: Creator add getDispatcher

--- bindings/include/VkBindings/private/Creator.hpp
+++ bindings/include/VkBindings/private/Creator.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
+#include "VkBindings/private/Loader.hpp"
+
 #include <utility>
+
 namespace VkBindings::impl_Objects {
 struct Creator {
     template <typename T, typename... Ts> static auto create(Ts &&...arg) -> T {
         return T{std::forward<Ts>(arg)...};
+    }
+    template <typename T>
+    static auto getDispatcher(const T &obj) -> const impl_Loader::Dispatcher & {
+        return obj.getDispatcher();
     }
 };
 } // namespace VkBindings::impl_Objects
